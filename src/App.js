7d4c149c2df2b09ee4c43c06789091500afa2f0b
Alice Hsing <alicehsing@gmail.com>
2022-02-18 11:42:30 -0800

    set up AuthPage containing signin/signup form for users to login. Once log in, redirect to the /SearchPage

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from './logo.svg';import './App.css';
+import { useState } from 'react';
+import { 
+  BrowserRouter as Router,
+  NavLink,
+  Switch,
+  Route,
+  Redirect
+} from 'react-router-dom';
+import { logout } from './services/fetch-utils';
+import AuthPage from './AuthPage';
+import SearchPage from './SearchPage';
 
-function App() {
+export default function App() {
+  //set the initial state of the user to come from localStorage, which is where supabase puts the token
+  //then we don't have to worry about losing our token state in react and ending with localStorage
+  //and our react state getting out of sync
+  const [currentUser, setCurrentUser] = useState(localStorage.getItem('supabase.auth.token'));
+
+  async function handleLogout() {
+    await logout();
+    setCurrentUser('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          {
+            currentUser && 
+          <>
+            <NavLink activeClassName="my-active-class" to="/search">Search Movie</NavLink>
+            <NavLink activeClassName="my-active-class" to="/watchlist">Watchlist Page</NavLink>
+            <button onClick={handleLogout}>Logout</button>
+          </>
+          }
+        </header>
+        <main>
+          <Switch>
+            <Route exact path="/">
+              {
+                currentUser
+                  ? <Redirect to="/search" />
+                  : <AuthPage setCurrentUser={setCurrentUser} />
+              }
+            </Route>
+            <Route exact path="/search">
+              {
+                !currentUser
+                  ? <Redirect to="/" />
+                  : <SearchPage />
+              }
+            </Route>
+
+          </Switch>
+        </main>
+      </div>
+    </Router>
+    
   );
 }
 
-export default App;
+
---
+++ src/services/fetch-utils.js
@@ -0,0 +1,21 @@
+import { client, checkError } from './client';
+
+
+export async function signUpUser(email, password) {
+  const response = await client.auth.signUp({ email, password });
+  return response.user;
+}
+
+export async function signInUser(email, password) {
+  const response = await client.auth.signIn({ email, password });
+  return response.user;
+}
+
+export function getUser() {
+  return client.auth.session() && client.auth.session().user;
+}
+
+export async function logout() {
+  await client.auth.signOut();
+  return (window.location.href = '../');
+}
