e03e0b39103b4f891461ac8f3ae73a6a4b766a7e
Alice Hsing <alicehsing@gmail.com>
2022-02-18 17:27:55 -0800

    add style to movie item

--- src/Movie.js
+++ src/Movie.js
@@ -4,7 +4,7 @@ export default function Movie({ movie }) {
   return (
     <div className='movie-item'>
       <h3>{movie.title}</h3>
-      <p>{movie.release_date}</p>
+      <p>Release Date: {movie.release_date}</p>
       <p>{movie.overview}</p>
       <p>
         <img src={movie.poster_path ? `https://image.tmdb.org/t/p/original${movie.poster_path}` : `https://placekitten.com/200/300`} />;
